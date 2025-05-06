73628f90c4dddfe9cc226b4b4a9f58a7c781c3c7
Avinash <avinashparmar83503@gmail.com>
2025-05-06 15:32:47 +0530

    changes

--- main.js
+++ main.js
@@ -56,7 +56,20 @@ const swiper = new Swiper(".swiper", {
   slidesPerView: 2,
   spaceBetween: 20,
   loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
 });
+
 
 ScrollReveal().reveal(".subscribe .section__header", {
   ...scrollRevealOption,
